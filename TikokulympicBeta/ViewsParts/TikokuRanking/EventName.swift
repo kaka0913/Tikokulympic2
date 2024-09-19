d078c11e7689f6cca466e135ba0c346d12f0eaf3
kaka0913 <kabuyuu0913@gmail.com>
2024-09-20 06:04:52 +0900

    差分

---
+++ TikokulympicBeta/ViewsParts/TikokuRanking/EventName.swift
@@ -0,0 +1,18 @@
+//
+//  EventName.swift
+//  TikokulympicBeta
+//
+//  Created by 株丹優一郎 on 2024/09/20.
+//
+
+import SwiftUI
+
+struct EventName: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventName()
+}
---
+++ TikokulympicBeta/ViewsParts/TikokuRanking/TextSegmentControll.swift
@@ -0,0 +1,50 @@
+//
+//  TextSegmentControll.swift
+//  TikokulympicBeta
+//
+//  Created by 株丹優一郎 on 2024/09/20.
+//
+
+import SwiftUI
+
+struct TextSegmentedControl: View {
+    @State private var selectedIndex: Int = 0
+
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(0..<2) { index in
+                Text(index == 0 ? "到着者" : "ランキング")
+                    .font(index == 0 ? .system(size: 24) : .system(size: 24))
+                    .fontWeight(.bold)
+                    .frame(width: 115)
+                    .padding(.horizontal, 40)
+                    .padding(.vertical, 20)
+                    .background(self.selectedIndex == index ? Color.darkred : Color.lightgray)
+                    .foregroundColor(self.selectedIndex == index ? Color.white : Color.black)
+                    .clipShape(
+                        index == 0
+                            ? RoundedCornerShape(corners: [.topLeft, .bottomLeft], radius: 40)
+                            : RoundedCornerShape(corners: [.topRight, .bottomRight], radius: 40)
+                    )
+                    .onTapGesture {
+                        self.selectedIndex = index
+                    }
+            }
+        }
+    }
+}
+
+struct RoundedCornerShape: Shape {
+    var corners: UIRectCorner
+    var radius: CGFloat
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(
+            roundedRect: rect, byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+#Preview {
+    TextSegmentedControl()
+}
