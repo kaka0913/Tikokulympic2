4ef24beccbbf885ee5f9f6134e430d6500828b1b
kaka0913 <kabuyuu0913@gmail.com>
2024-09-24 21:30:18 +0900

    プロフィールビューの修正

--- TikokulympicBeta/Views/Profile/ProfileView.swift
+++ TikokulympicBeta/Views/Profile/ProfileView.swift
@@ -17,66 +17,81 @@ struct ProfileView: View {
     let title = "警察なのに遅刻"
 
     var body: some View {
-        VStack(spacing: 20) {
-            HStack {
-                Text("称号")
-                    .foregroundColor(.white)
-                    .font(.headline)
-                    .padding(.trailing, 8)
+            
+        VStack {
+            VStack(spacing: 0) {
+                VStack(alignment: .leading) {
+                    HStack {
+                        Spacer()
+                        
+                        Text("称号:")
+                            .foregroundColor(.white)
+                            .font(.system(size: 20))
 
-                Text(title)
-                    .foregroundColor(.white)
-                    .font(.headline)
-            }
-            .padding(.top, 16)
+                        Text(title)
+                            .foregroundColor(.white)
+                            .font(.system(size: 25))
+                            .bold()
+                        
+                        Spacer()
+                    }
+                    .padding(.bottom, 4)
 
-            VStack(spacing: 20) {
-                HStack(spacing: 16) {
-                    VStack(alignment: .leading, spacing: 8) {
-                        HStack {
-                            AuthProfileImage()
-                            Text("名前: \(userName)")
-                                .font(.title3)
-                            Image(systemName: "pencil")
-                        }
+                    Rectangle()
+                        .frame(height: 2)
+                        .padding(.horizontal, 40)
+                        .foregroundColor(.white)
+                    
+                }
+                    VStack(spacing: 15) {
+                        HStack(spacing: 16) {
+                            VStack(alignment: .leading, spacing: 15) {
+                                HStack {
+                                    AuthProfileImage()
+                                    Text("名前: \(userName)")
+                                        .font(.title3)
+                                    Image(systemName: "pencil")
+                                }
 
-                        VStack(alignment: .leading, spacing: 12) {
-                            HStack {
-                                Image(systemName: "clock.fill")
-                                Text("遅刻回数: \(lateCount)")
-                                    .font(.title2)
-                            }
+                                VStack(alignment: .leading, spacing: 15) {
+                                    HStack {
+                                        Image(systemName: "clock.fill")
+                                        Text("遅刻回数: \(lateCount)")
+                                            .font(.title2)
+                                    }
 
-                            HStack {
-                                Image(systemName: "hand.raised.fill")
-                                Text("間に合った回数: \(onTimeCount)")
-                                    .font(.title2)
-                            }
+                                    HStack {
+                                        Image(systemName: "hand.raised.fill")
+                                        Text("間に合った回数: \(onTimeCount)")
+                                            .font(.title2)
+                                    }
 
-                            HStack {
-                                Image(systemName: "hourglass.bottomhalf.fill")
-                                Text("総遅刻時間: \(totalLateTime)")
-                                    .font(.title2)
+                                    HStack {
+                                        Image(systemName: "hourglass.bottomhalf.fill")
+                                        Text("総遅刻時間: \(totalLateTime)")
+                                            .font(.title2)
+                                    }
+                                    Spacer()
+                                }
+                                .padding(.leading, 8)
                             }
                             Spacer()
                         }
+                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(15)
+                        .padding(.horizontal)
+                        Spacer()
                     }
-                    Spacer()
+                    .padding(.top, 20)
                 }
-                .padding()
-                .background(Color(.systemGray6))
-                .cornerRadius(15)
-                .padding(.horizontal)
-            }
-            .padding(.top, 20)
-
+            .background(Color.green)
+            
             Spacer()
         }
-        .background(Color.green)
-        .cornerRadius(20)
-        .padding(.all, 10)
     }
 }
+
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
