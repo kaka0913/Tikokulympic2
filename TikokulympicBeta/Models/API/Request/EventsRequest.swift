078a7f123fc02f92222e9aaecac93a28adaec836
Sawaki Shuto <swkshuto0208@icloud.com>
2024-09-29 13:21:23 +0900

    add: 必要なファイル追加

---
+++ TikokulympicBeta/Models/API/Request/EventsRequest.swift
@@ -0,0 +1,8 @@
+//
+//  EventsRequest.swift
+//  TikokulympicBeta
+//
+//  Created by 澤木柊斗 on 2024/09/26.
+//
+
+import Foundation
---
+++ TikokulympicBeta/Views/EventList/EventListCard.swift
@@ -0,0 +1,8 @@
+//
+//  EventListCard.swift
+//  TikokulympicBeta
+//
+//  Created by 澤木柊斗 on 2024/09/26.
+//
+
+import Foundation
