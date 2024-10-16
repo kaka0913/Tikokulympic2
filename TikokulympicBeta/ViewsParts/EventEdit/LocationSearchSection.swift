c76f05370357d843f21366826e59d6b22ac48460
unmy040915 <m.yuuta0915@icloud.com>
2024-10-16 19:47:04 +0900

    上げ直し

--- TikokulympicBeta/ViewsParts/EventEdit/LocationSearchSection.swift
+++ TikokulympicBeta/ViewsParts/EventEdit/LocationSearchSection.swift
@@ -1,22 +1,51 @@
-//
-//  LocationSearchSection.swift
-//  TikokulympicBeta
-//
-//  Created by 株丹優一郎 on 2024/09/26.
-//
-
 import SwiftUI
 
 struct LocationSearchSection: View {
+    @State private var predictions: [String] = []
     @Binding var searchQuery: String
+    private let apiKey = "API key"
     
     var body: some View {
         VStack(alignment: .leading, spacing: 8) {
             Text("地点登録")
                 .font(.system(size: 15))
                 .bold()
-            TextField("検索...", text: $searchQuery)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            TextField("検索...", text: $searchQuery, onEditingChanged: { _ in }, onCommit: {
+                self.performSearch()
+            })
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .onChange(of: searchQuery) {
+                self.performSearch()
+            }
+
+            
+            if !predictions.isEmpty {
+                List(predictions, id: \.self) { prediction in
+                    Text(prediction)
+                }
+                .frame(maxHeight: 150)
+            }
         }
     }
-}
+    func performSearch() {
+        guard let url = URL(string: "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=\(searchQuery)&key=\(apiKey)&types=(cities)") else { return }
+
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else { return }
+
+            do {
+                let result = try JSONDecoder().decode(PlaceAutocompleteResponse.self, from: data)
+                DispatchQueue.main.async {
+                    self.predictions = result.predictions.map { $0.description }
+                }
+            } catch {
+                print(error)
+            }
+        }
+        task.resume()
+    }
+    }
+
+
+
