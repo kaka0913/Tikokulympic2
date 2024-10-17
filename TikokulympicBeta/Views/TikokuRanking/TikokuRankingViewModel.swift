a00783d8d56453c8f063a9a49672e6925ab4c730
kaka0913 <kabuyuu0913@gmail.com>
2024-10-17 18:41:17 +0900

    遅刻ランキング周りのweb socketの導入

--- TikokulympicBeta/Views/TikokuRanking/TikokuRankingViewModel.swift
+++ TikokulympicBeta/Views/TikokuRanking/TikokuRankingViewModel.swift
@@ -11,30 +11,50 @@ import SwiftUI
 
 class TikokuRankingViewModel: ObservableObject {
     @Published var userRankings: [UserRankingData] = []
-    @AppStorage("latitude") var latitude: Double = 0.0
-    @AppStorage("longitude") var longitude: Double = 0.0
-   
-    private let locationManager = CLLocationManager()
-    private var destinationLocation: CLLocation?
-    
+    @Published var isTikokulympicFinished: Bool = false
+    private var rankingService = TikokuRankingService.shared
+    private var messageTask: Task<Void, Never>?
+    private var rankingRequestTask: Task<Void, Never>?
+
     init() {
-        destinationLocation = CLLocation(latitude: latitude, longitude: longitude)
-        setupLocationManager()
-        setupMockData()
+        messageTask = Task {
+            await listenForMessages()
+        }
     }
     
-    private func setupLocationManager() {
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
+    deinit {
+        messageTask?.cancel()
     }
     
-    private func setupMockData() {
-        userRankings = [
-            UserRankingData(id: 1,  position: 1, name: "しゅうと", alias: "遅刻王", distance: 0),
-            UserRankingData(id: 51, position: 2, name: "ゆうた", alias: "ねぼう王", distance: 0),
-            UserRankingData(id: 21, position: 3, name: "しょうま", alias: "ねぼう王", distance: 0),
-            UserRankingData(id: 31, position: 4, name: "かぶたん", alias: "遅刻王", distance: 0),
-            UserRankingData(id: 61, position: 5, name: "ゆいぴ", alias: "しらふ酔王", distance: 0)
-        ]
+    func requestLatestRanking() {
+        rankingRequestTask?.cancel() // 既存のタスクがあればキャンセル
+        rankingRequestTask = Task {
+            // WebSocketの接続が確立されるまで待機
+            while !WebSocketClient.shared.isConnected {
+                try? await Task.sleep(nanoseconds: 100_000_000) // 100ミリ秒待機
+            }
+            // 接続が確立されたら最新のランキングをリクエスト
+            rankingService.requestLatestRanking()
+        }
+    }
+
+    private func listenForMessages() async {
+        let messageStream = rankingService.messageStream()
+        for await message in messageStream {
+            switch message {
+            case .rankingUpdate(let rankings):
+                DispatchQueue.main.async {
+                    self.userRankings = rankings
+                }
+            case .tikokulympicFinished(let message):
+                DispatchQueue.main.async {
+                    self.isTikokulympicFinished = true
+                }
+                // ループを抜けてタスクを終了
+                break
+            }
+        }
+        // ストリームが終了したため WebSocket を切断
+        WebSocketClient.shared.disconnect()
     }
 }
